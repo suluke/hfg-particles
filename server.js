f305f5b9f9741ba07ec9249a75ec5f0ffd251e40
Lukas Böhm <lukas.boehm93@gmail.com>
2017-05-03 00:08:48 +0200

    Initial commit
    Sets up a small dev environment and some boilerplate code

---
+++ server.js
@@ -0,0 +1,55 @@
+const express         = require('express');
+const rollup          = require('express-middleware-rollup');
+const sass            = require('node-sass-middleware');
+const buble           = require('rollup-plugin-buble');
+const fs              = require('fs-promise');
+
+const path            = require('path');
+const PkgRoot         = __dirname;
+const StaticDir       = 'static'; // must not be absolute since concatenated with `root` option
+
+// Javascript compilation
+const js = rollup({
+  src: 'js',
+  dest: StaticDir,
+  root: PkgRoot,
+  prefix: '/',
+  serve: 'on-compile',
+  rollupOpts: {
+    plugins: [buble()]
+  }
+});
+
+// Css compilation
+const css = sass({
+  src: 'sass',
+  dest: StaticDir,
+  root: PkgRoot,
+  prefix: '/',
+  outputStyle: 'extended',
+  includePaths: [
+    path.join(PkgRoot, 'node_modules')
+  ]
+});
+
+// Static file server
+const StaticPath = path.join(PkgRoot, StaticDir);
+const index   = (req, res) => {
+  res.sendFile(path.join(PkgRoot, 'index.html'));
+};
+const statics = express.static(StaticPath);
+
+fs.mkdirp(StaticPath)
+.then(() => fs.copy(path.join(PkgRoot, 'node_modules', 'font-awesome', 'fonts'), path.join(StaticPath, 'fonts')))
+.then(() => {
+  const server = express();
+  server.get('/', index);
+  server.use(js);
+  server.use(css);
+  server.use(statics);
+
+  server.listen(3000);
+})
+.catch(err => {
+  console.error(err)
+});
