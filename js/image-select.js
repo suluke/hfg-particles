8ca116ad77fb96694364a4763f8d327145f575d0
Lukas Böhm <suluke93@gmail.com>
2017-05-04 20:16:25 +0200

    Fix drag-n-drop implementation
    The whole file-select code was overhauled and feels very natural to use.
    Styling is just 'ok' right now and we can do better here
    
    fix #2

---
+++ js/image-select.js
@@ -0,0 +1,44 @@
+if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
+  throw new Error('The File APIs are not fully supported in this browser.');
+}
+
+export default class ImgSelect {
+  constructor() {
+    // properties:
+    this.changeListeners = [];
+
+    // drag-n-drop support
+    const html = document.documentElement;
+    const input = document.getElementById('btn-file-select');
+    const dragClass = 'dragging-file';
+    const dragenter = e => {
+      html.classList.add(dragClass);
+      e.stopPropagation();
+      e.preventDefault();
+    }
+    const dragleave = e => {
+      html.classList.remove(dragClass);
+      e.stopPropagation();
+      e.preventDefault();
+    }
+
+    html.addEventListener("dragenter", dragenter, false);
+    input.addEventListener("dragleave", dragleave, false);
+    // if we preventDefault on drop, the change event will not fire
+    input.addEventListener("drop", (e) => { html.classList.remove(dragClass) }, false);
+
+    // catch the change event
+    const handleFileSelect = (evt) => {
+      let file = null;
+      if (evt.target.files.length > 0) {
+        file = evt.target.files[0];
+      }
+      this.changeListeners.forEach(listener => listener(file));
+    };
+    input.addEventListener('change', handleFileSelect);
+  }
+
+  addChangeListener(listener) {
+    this.changeListeners.push(listener);
+  }
+};
