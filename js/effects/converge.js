583829a5378da6a0a0285726d67bcb667abab273
luckyxxl <mail@luckyxxl.de>
2017-05-22 10:54:48 +0200

    Use static effect descriptions for effect code generation instead of object-oriented approach. Implement per-instance uniforms.

--- js/effects/converge.js
+++ js/effects/converge.js
@@ -61,56 +61,61 @@ class ConvergeConfigUI extends ConfigUI {
 }
 
 export default class ConvergeEffect extends Effect {
-  static insertIntoVertexShader(vertexShader, instance) {
-    // eslint-disable-next-line no-param-reassign, prefer-template
-    vertexShader.uniforms += `
-      uniform float convergeTime;
-      uniform float convergeSpeed;
-      uniform float convergeRotationSpeed;
-      uniform float convergeMaxTravelTime;
-    `;
-    // eslint-disable-next-line no-param-reassign, prefer-template
-    vertexShader.mainBody += `
-      {
-        vec2 screenTarget = ` + {
-          center:        'vec2(0., 0.)',
-          'color wheel': 'getDirectionVector(hsv[0] + convergeTime * convergeRotationSpeed) * vec2(.8) * vec2(invScreenAspectRatio, 1.)'
-        }[instance.config.target] + `;
+  static getUniforms() {
+    return [{
+        name: 'convergeTime',
+        type: 'float',
+        getValue: (instance) => function(ctx) {
+          const period = 2 * Math.sqrt(2 / instance.config.speed);
+
+          return fract(ctx.time / period) * period;
+        }
+      }, {
+        name: 'convergeSpeed',
+        type: 'float',
+        getValue: (instance) => instance.config.speed
+      }, {
+        name: 'convergeRotationSpeed',
+        type: 'float',
+        getValue: (instance) => instance.config.rotationSpeed
+      }, {
+        name: 'convergeMaxTravelTime',
+        type: 'float',
+        getValue: (instance) => Math.sqrt(2 / instance.config.speed)
+      }
+    ];
+  }
+
+  static getCode() {
+    const u = Effect.createUniformToken;
+
+    return [`{
+        vec2 screenTarget = `, (instance) => {
+          return {
+            center:        'vec2(0., 0.)',
+            'color wheel': [ 'getDirectionVector(hsv[0] + ', u('convergeTime'), ' * ', u('convergeRotationSpeed'), ') * vec2(.8) * vec2(invScreenAspectRatio, 1.)' ]
+          }[instance.config.target];
+        }, `;
         vec2 target = (invViewProjectionMatrix * vec4(screenTarget, 0, 1)).xy;
 
         vec2 d = target - initialPosition.xy;
         float d_len = length(d);
-        
-        float stop_t = sqrt(2. * d_len / convergeSpeed);
 
-        if(convergeTime < stop_t) {
-          float t = min(convergeTime, stop_t);
-          position.xy += .5 * d / d_len * convergeSpeed * t * t;
-        } else if(convergeTime < convergeMaxTravelTime) {
+        float stop_t = sqrt(2. * d_len / `, u('convergeSpeed'), `);
+
+        if(`, u('convergeTime'), ` < stop_t) {
+          float t = min(`, u('convergeTime'), `, stop_t);
+          position.xy += .5 * d / d_len * `, u('convergeSpeed'), ` * t * t;
+        } else if(`, u('convergeTime'), ` < `, u('convergeMaxTravelTime'), `) {
           position.xy += d;
         } else {
-          float t = convergeTime - convergeMaxTravelTime;
+          float t = `, u('convergeTime'), ` - `, u('convergeMaxTravelTime'), `;
           //position.xy += mix(d, vec2(0.), 1. - (1.-t) * (1.-t));
           //position.xy += mix(d, vec2(0.), t * t);
-          position.xy += mix(d, vec2(0.), -cos(t / convergeMaxTravelTime * PI) * .5 + .5);
+          position.xy += mix(d, vec2(0.), -cos(t / `, u('convergeMaxTravelTime'), ` * PI) * .5 + .5);
         }
-      }
-    `;
-  }
-
-  static insertUniforms(uniforms, instance) {
-    // eslint-disable-next-line no-param-reassign
-    uniforms.convergeTime = (ctx) => {
-      const period = 2 * Math.sqrt(2 / instance.config.speed);
-
-      return fract(ctx.time / period) * period;
-    };
-    // eslint-disable-next-line no-param-reassign
-    uniforms.convergeSpeed = () => instance.config.speed;
-    // eslint-disable-next-line no-param-reassign
-    uniforms.convergeRotationSpeed = () => instance.config.rotationSpeed;
-    // eslint-disable-next-line no-param-reassign
-    uniforms.convergeMaxTravelTime = () => Math.sqrt(2 / instance.config.speed);
+      }`
+    ];
   }
 
   static getDisplayName() {
