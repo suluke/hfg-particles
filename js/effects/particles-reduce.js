269fe9918be8bdcb54b5f2862ae13a50060ba5eb
Lukas Böhm <suluke93@gmail.com>
2017-10-17 20:37:50 +0200

    Implement ParticlesReduceEffect

---
+++ js/effects/particles-reduce.js
@@ -0,0 +1,128 @@
+import Effect, { ConfigUI, fract } from './effect';
+import { parseHtml } from '../ui/util';
+import Ease from './ease-mixins';
+
+const EffectName = 'Reduce Particle Count';
+const EffectDescription = 'Drops the given amount of particles';
+
+class ParticlesReduceConfigUI extends ConfigUI {
+  constructor() {
+    super();
+    const classPrefix = 'effect-particles-reduce';
+    this.element = parseHtml(`
+      <fieldset>
+        <legend>${EffectName}</legend>
+        Remaining particles amount: <input class="${classPrefix}-reduction-amount" type="number" min="0" max="100" step="1" value="100"/>%
+        <br>
+      </fieldset>
+    `);
+    const ui = this.element;
+    this.reductionAmountInput = ui.querySelector(`.${classPrefix}-reduction-amount`);
+
+    this.reductionAmountInput.addEventListener('change', () => {
+      this.notifyChange();
+    });
+
+    Ease.extend(this, classPrefix);
+  }
+
+  getElement() {
+    return this.element;
+  }
+
+  getConfig() {
+    return {
+      amount: parseFloat(this.reductionAmountInput.value) / 100
+    };
+  }
+
+  applyConfig(config) {
+    this.reductionAmountInput.value = config.amount * 100;
+  }
+}
+
+export default class ParticlesReduceEffect extends Effect {
+  static register(instance, props, uniforms, vertexShader) {
+    const amount = instance.config.amount;
+    if (amount < 1) {
+      // This works as follows:
+      // We assume we want to render a different particle grid which is
+      // coarser than the one defined by {x,y}ParticlesCount but with a
+      // similar aspect ratio. Since it's cells are bigger than the cells
+      // in the original particle grid when rendered onto the same area,
+      // we refer to it as "super-grid"
+      const px = props.config.xParticlesCount;
+      const py = props.config.yParticlesCount;
+      const AR =  px / py;
+      let newH = Math.sqrt(amount * py * py);
+      const newW = Math.round(newH * AR);
+      newH = Math.round(newH);
+      // Now we know the super-grid's dimensions newW and newH. We can
+      // also calculate the grid's cell's dimensions relative to the old
+      // grid
+      const cellSize = Math.min(px / newW, Math.max(px, py)); // FIXME handle 0% better
+
+      const easeFunc = Ease.setupShaderEasing(instance, uniforms);
+
+      vertexShader.mainBody += `
+        float ease = ${easeFunc};
+        float cellSize = (1. - ease) + (ease * float(${cellSize}));
+        // subcellMid is the center of the sub-cell occupied by the current
+        // vertex (a.k.a. particle)
+        vec2 subcellMid = initialPosition.xy * vec2(float(${px}), float(${py}));
+        // subcell is the 2d coord of this vertex' sub-cell
+        vec2 subcell = floor(subcellMid);
+        // translate the mid into super-grid coordinates
+        vec2 cellMid = subcellMid / vec2(cellSize);
+        // calculate the 2d coords of the super-cell this vertex falls into
+        vec2 cell = floor(cellMid);
+        // top left (TL) + bottom right (BR) of the super-cell in original grid coords
+        vec2 TL = (cellMid - vec2(0.5)) * vec2(cellSize); 
+        vec2 BR = (cellMid + vec2(0.5)) * vec2(cellSize);
+        // Now we calculate the sub-cells that contain the super-cell's TL and BR 
+        vec2 subcellTL = floor(TL);
+        vec2 subcellBR = floor(BR);
+        vec2 centerSubcell = mix(subcellTL, subcellBR, 0.5);
+        // a sub-cell dominates a super-cell iff it's the centerSubcell
+        bool dominatesCell = all(equal(subcell, centerSubcell));
+        if (dominatesCell) {
+          // TODO This should just result in an offset
+          // The dominating cell's position should now be set to the center
+          // of the super-cell it dominates
+          position.xy = (cell + vec2(0.5)) * cellSize / vec2(float(${px}), float(${py}));
+        } else {
+          pointSize = 0.;
+          rgb = vec3(0.);
+        }
+      `;
+    } 
+  }
+
+  static getDisplayName() {
+    return EffectName;
+  }
+
+  static getDescription() {
+    return EffectDescription;
+  }
+
+  static getConfigUI() {
+    if (!this._configUI) {
+      this._configUI = new ParticlesReduceConfigUI();
+    }
+
+    return this._configUI;
+  }
+
+  static getDefaultConfig() {
+    return {
+      amount: 0.5
+    };
+  }
+
+  static getRandomConfig() {
+    return {
+      amount: Math.random()
+    };
+  }
+}
