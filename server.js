0b14018d3c721fe6a0647b468a86229400859a5d
Lukas Böhm <suluke93@gmail.com>
2018-02-14 20:37:02 +0100

    Pull in ffmpeg.js and update server to be able to use it

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const resolve         = require('rollup-plugin-node-resolve');
 const commonjs        = require('rollup-plugin-commonjs');
 const replace         = require('rollup-plugin-replace');
 const json            = require('rollup-plugin-json');
+const string          = require('rollup-plugin-string');
 
 const git             = require('git-rev');
 
@@ -15,10 +16,13 @@ const PkgRoot         = __dirname;
 const StaticDir       = 'static'; // must not be absolute since concatenated with `root` option
 const StaticPath      = path.join(PkgRoot, StaticDir);
 
-fs.mkdirp(StaticPath)
-.then(() => fs.copy(path.join(PkgRoot, 'node_modules', 'font-awesome', 'fonts'), path.join(StaticPath, 'fonts')))
-.then(() => new Promise((res, rej) => git.short(res)))
-.then((gitrev) => {
+fs.mkdirp(StaticPath).then(
+  () => fs.copy(path.join(PkgRoot, 'node_modules', 'font-awesome', 'fonts'), path.join(StaticPath, 'fonts'))
+).then(
+  () => fs.copy(path.join(PkgRoot, 'node_modules', 'ffmpeg.js', 'ffmpeg-worker-mp4.js'), path.join(StaticPath, 'ffmpeg-worker-mp4.js'))
+).then(
+  () => new Promise((res, rej) => git.short(res))
+).then((gitrev) => {
   // Javascript compilation
   const js = rollup({
     src: 'js',
@@ -28,7 +32,11 @@ fs.mkdirp(StaticPath)
     serve: 'on-compile',
     rollupOpts: {
       plugins: [
+        // turn file contents into js strings which can be imported
+        string({ include: '**/*.md' }),
+        // allow importing json as ES6 modules
         json(),
+        // replace patterns in files with new values
         replace({
           include: 'js/config.js',
           delimiters: [ '<@', '@>' ],
@@ -37,13 +45,19 @@ fs.mkdirp(StaticPath)
             GIT_REV: gitrev
           }
         }),
-        buble(),
+        // ES6 -> ES5
+        buble({
+          exclude: [ 'node_modules/ffmpeg.js/**' ]
+        }),
+        // use Node resolution algorithm to find files in node_modules
         resolve({
           main: true,
           extensions: [ '.js', '.json' ]
         }),
+        // CommonJS modules -> ES6 modules
         commonjs({
           extensions: [ '.js', '.json' ],
+          exclude: [ 'node_modules/ffmpeg.js/**' ],
           namedExports: {
             'node_modules/image-capture/lib/imagecapture.js': [ 'ImageCapture' ]
           }
@@ -78,7 +92,7 @@ fs.mkdirp(StaticPath)
   server.use(statics);
 
   server.listen(3000);
-})
-.catch(err => {
+}).catch(err => {
   console.error(err)
+  process.exit(1);
 });
